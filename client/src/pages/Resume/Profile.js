31bcf3853761b0431d053f2c99e777af137506c1
Nida.Q <nida@emamilid@gmail.com>
2020-08-25 19:03:33 -0400

    Profile updated

--- client/src/pages/Resume/Profile.js
+++ client/src/pages/Resume/Profile.js
@@ -2,50 +2,58 @@ import React, { useContext } from 'react';
 import "./forms.css"
 export default function Profile(props) {
   return (
-        <form
-          className="flex signup-form"
-          style={{ paddingLeft: "0px" }}
-        >
-          <div className="content flex flex-column justify-center items-center">
-            <div className="text">
-              <h1>Create Achievements</h1>
-            </div>
-            <div className="form-group">
-              <input
-                type="text"
-                placeholder="Enter your username"
-                name="userName"
-              />
-           <section className="userValidation">error</section>
-            </div>
-            <div className="form-group">
-              <input
-                type="email"
-                placeholder="someone@email.com"
-                name="email"
-              />
-              <section className="userValidation">error</section>
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                placeholder="Password"
-                name="password"
-              />
-              <section className="userValidation">error</section>
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                placeholder="Confirm Password"
-                name="confirmPassword"
-              />
-              <section className="userValidation">error</section>
-            </div>
-            <div className="form-group">
-              <input  type="submit" value="SIGNUP" />
-            </div>
-          </div>
-        </form>
+    <form
+    className="flex signup-form"
+    style={{ paddingLeft: "0px" }}
+  >
+    <div className="content flex flex-column justify-center items-center">
+      <div className="text">
+        <h1>Information about you that highlights on top of resume</h1>
+      </div>
+      <div className="form-group">
+        <input
+          type="text"
+          placeholder="Enter your job role or professional title. e.g Full Stack Developer"
+          name="prof_title"
+        />
+     <section className="userValidation">error</section>
+      </div>
+      <div className="form-group">
+        <input
+          type="text"
+          placeholder="Enter your first name."
+          name="first_name"
+        />
+        <section className="userValidation">error</section>
+      </div>
+      <div className="form-group">
+        <input
+          type="text"
+          placeholder="Enter your last name"
+          name="last_name"
+        />
+        <section className="userValidation">error</section>
+      </div>
+      <div className="form-group">
+        <input
+          type="email"
+          placeholder="Enter your email"
+          name="email"
+        />
+        <section className="userValidation">error</section>
+      </div>
+      <div className="form-group">
+        <input
+          type="number"
+          placeholder="Enter your phone number"
+          name="phone_number"
+        />
+        <section className="userValidation">error</section>
+      </div>
+      <div className="form-group">
+        <input  type="submit" value="SIGNUP" />
+      </div>
+    </div>
+  </form>
   )
 }
