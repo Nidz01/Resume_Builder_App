fe04655ca595e3d3c6cf2a70c5f0c2c06a00bef1
Nida.Q <nida@emamilid@gmail.com>
2020-08-25 18:38:20 -0400

    resume child component styling

--- client/src/pages/Resume/Experience.js
+++ client/src/pages/Resume/Experience.js
@@ -8,37 +8,50 @@ export default function Experience(props) {
         >
           <div className="content flex flex-column justify-center items-center">
             <div className="text">
-              <h1>Create Achievements</h1>
+              <h1>Your histrory of professional experience</h1>
             </div>
             <div className="form-group">
               <input
                 type="text"
-                placeholder="Enter your username"
-                name="userName"
+                placeholder="Enter your job title"
+                name="job_Title"
               />
            <section className="userValidation">error</section>
             </div>
             <div className="form-group">
               <input
-                type="email"
-                placeholder="someone@email.com"
-                name="email"
+                type="text"
+                placeholder="Enter the institute or organization that you worked for under the above job title"
+                name="organization"
               />
               <section className="userValidation">error</section>
             </div>
             <div className="form-group">
               <input
-                type="password"
-                placeholder="Password"
-                name="password"
+                type="text"
+                placeholder="Enter the address of above mentioned organization"
+                name="job_location"
               />
               <section className="userValidation">error</section>
             </div>
             <div className="form-group">
               <input
-                type="password"
-                placeholder="Confirm Password"
-                name="confirmPassword"
+                type="month"
+                placeholder="Enter the start date"
+                name="job_start"
+              />
+              <input
+                type="month"
+                placeholder="Enter the end date"
+                name="job_end"
+              />
+              <section className="userValidation">error</section>
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Enter one or two sentences to describe the nature of company"
+                name="company_Profile"
               />
               <section className="userValidation">error</section>
             </div>
