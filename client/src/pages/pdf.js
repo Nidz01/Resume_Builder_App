2bf39f0c61cf2fe6fc2bb014307f1090284686d4
Margarita Struts <margarita.struts@me.com>
2020-09-02 01:49:28 -0400

    Building PDF

--- client/src/pages/pdf.js
+++ client/src/pages/pdf.js
@@ -1,84 +1,104 @@
-// import React from "react";
-// import { PDFDownloadLink, Document, Page, Text, View, StyleSheet, PDFViewer } from "@react-pdf/renderer";
-
-// export default function PDF (props) {
-
-
-//     const styles = StyleSheet.create({
-//             page: {
-//                 flexDirection: "row"
-//             },
-//             section: {
-//                 flexGrow: 1
-//             }
-//     });
-
-//     const MyDocument = () => (
-//         <Document>
-//                 <Page size="A4" style={styles.page}>
-//                 <View style={styles.section}>
-//                     <Text>Hello World!</Text>
-//                 </View>
-//                 <View style={styles.section}>
-//                     <Text>We're inside a PDF!</Text>
-//                 </View>
-//                 </Page>
-//             </Document>
-//         )
-       
-        
-//         return(
-//           <div style={styles.container}>
-//             <PDFViewer>
-//                 <MyDocument/>
-//             </PDFViewer> 
-//             {/* {
-// //             isResumeCompleted(props.ResumeState) && 
-// //             <PDFDownloadLink document={<MyDocument />} fileName="somename.pdf">
-// //               {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
-// //             </PDFDownloadLink>
-// // } */}
-//           </div>
-    
-//         );
-// }
-
 import React from "react";
-import ReactDOM, { render } from 'react-dom';
-import { PDFDownloadLink, Document, Page, Text, View, StyleSheet, PDFViewer } from "@react-pdf/renderer";
-import Button from 'react-bootstrap/Button';
+import { Document, Page, Text, View, StyleSheet, PDFViewer, Font } from "@react-pdf/renderer";
+import Header from '../pdf/Header';
 
 export default function pdfMaker () {
+    
     const styles = StyleSheet.create({
-        page: {
-            flexDirection: "row",
-            section: { color: 'white', textAlign: 'left', margin: 30 }
-        },
-        section: {
-            flexGrow: 1
-        },
         pdfbody: {
             width: 800,
             height: 720
-        }
-    });
-    const MyDocument = () => (
-        <Document>
-            <Page size="A4" style={styles.page}>
-            <View style={styles.section}>
-                <Text>Hello World!</Text>
-            </View>
-            <View style={styles.section}>
-                <Text>We're inside a PDF!</Text>
-            </View>
-            </Page>
-        </Document>
+        },
+        page: {
+          padding: 30,
+        },
+        container: {
+          flex: 1,
+          flexDirection: 'row',
+          '@media max-width: 400': {
+            flexDirection: 'column',
+          },
+        },
+        image: {
+          marginBottom: 10,
+        },
+        leftColumn: {
+          flexDirection: 'column',
+          width: 170,
+          paddingTop: 30,
+          paddingRight: 15,
+          '@media max-width: 400': {
+            width: '100%',
+            paddingRight: 0,
+          },
+          '@media orientation: landscape': {
+            width: 200,
+          },
+        },
+        footer: {
+          fontSize: 12,
+          //fontFamily: 'Lato Bold',
+          textAlign: 'center',
+          marginTop: 25,
+          paddingTop: 10,
+          borderWidth: 3,
+          borderColor: 'gray',
+          borderStyle: 'dashed',
+          '@media orientation: landscape': {
+            marginTop: 10,
+          },
+        },
+      });
+      
+      Font.register( {
+        family: 'Open Sans',
+        src: `${__dirname}/fonts/fonts/Open_Sans/OpenSans-Regular.ttf`,
+      });
+      Font.register( {
+        family: 'Lato',
+        src: `${__dirname}/fonts/fonts/Lato/Lato-Regular.ttf`,
+      });
+      Font.register( {
+        family: 'Lato Italic',
+        src: `${__dirname}/fonts/fonts/Lato/Lato-Italic.ttf`,
+      });
+      Font.register( {
+        family: 'Lato Bold',
+        src: `${__dirname}/fonts/fonts/Lato/Lato-Bold.ttf`,
+      });
+
+    const Resume = (props) => (
+        <Page {...props} style={styles.page}>
+        <Header/>
+        <View style={styles.container}>
+          <View style={styles.leftColumn}>
+            {/* <Image
+              src="https://react-pdf.org/static/images/luke.jpg"
+              style={styles.image}
+            /> */}
+          </View>
+        </View>
+        <Text style={styles.footer}>This IS the candidate you are looking for</Text>
+      </Page>
     )
+
+
+    const Output = () => (
+        <Document
+          author="Luke Skywalker"
+          keywords="awesome, resume, start wars"
+          subject="The resume of Luke Skywalker"
+          title="Resume"
+        >
+          <Resume size="A4" />
+        </Document>
+      );
+
     return(
     <header>
     <main className="container flex flex-column items-center justify-center signup">
       <div style={{display: 'flex', flexDirection: 'row', justifyContent: "space-between", alignItems: "center"}}>
-        <PDFViewer style={styles.pdfbody}><MyDocument/></PDFViewer>
+        <PDFViewer style={styles.pdfbody}><Output /></PDFViewer>
       </div>
     </main>
     </header>
